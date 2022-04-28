8c97202d77aec8785e59380854b9756cec353103
SohamMestry <73739689+SohamMestry@users.noreply.github.com>
2022-04-28 02:26:31 -0700

    Add files via upload

---
+++ C-124/practise.py
@@ -0,0 +1,54 @@
+from flask import Flask,jsonify,request
+#jsonify coverts JSON to readable format(string)
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+#@ is a decorater. / is the root path(home page).Method is bydefault GET
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/add-data",methods=["POST"])
+def add_task():
+    if not request.json:
+        return jsonify({
+            "status":"error",
+            "message": "Please provide the data!"
+        },400)
+
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({
+        "status":"success",
+        "message": "Task added succesfully!"
+    })
+
+@app.route("/get-data")
+def get_task():
+    return jsonify({
+        "data" : tasks
+    })
+
+if(__name__ == "__main__"):
+    #reloads the page if any changes happens
+    app.run(debug=True)
